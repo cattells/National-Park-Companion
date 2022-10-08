58f26b2f9e155e0951918b2655918cd8e42b05af
brandonO91 <brandon.oyama.0526@gmail.com>
2022-10-07 23:23:08 -0700

    adding html to template directory

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # def home():
 #     return "1st Place current status"
 
+# /views not necessary, can also be "/" etc
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':

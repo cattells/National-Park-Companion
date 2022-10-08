a2b5595758360cb3354a07973a9f33256c10922a
brandonO91 <brandon.oyama.0526@gmail.com>
2022-10-07 23:34:54 -0700

    Adding variable outline

--- views.py
+++ views.py
@@ -4,4 +4,9 @@ views = Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    # varEx - variables can be passed to template
+    return render_template("index.html", varEx="Hot Springs please")
+
+@views.route("/profile/<userName>")
+def profile(userName):
+    return render_template("index.html", varEx="Who likes BJJ??", name=userName)

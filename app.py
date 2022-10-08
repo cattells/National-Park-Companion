34b54e6371c27551e940e76d2e319b7f63bf2efe
brandonO91 <brandon.oyama.0526@gmail.com>
2022-10-07 22:54:52 -0700

    First route added

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def home():
+    return "1st Place current status"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

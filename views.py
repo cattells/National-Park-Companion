fcc3fdcd894d9a43cbf381b09940544e007f43cf
brandonO91 <brandon.oyama.0526@gmail.com>
2022-10-08 22:58:01 -0700

    adding script to html connection

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request, redirect, url_for
 
 views = Blueprint(__name__,"views")
 
@@ -10,3 +10,17 @@ def home():
 @views.route("/profile/<userName>")
 def profile(userName):
     return render_template("index.html", varEx="Who likes BJJ??", name=userName)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'wash' : 'brandon', 'tex' : 'gord'})
+
+# when sending data to the below end point route
+# @views.route("/data")
+# def get_data():
+#     data = request.json
+#     return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
